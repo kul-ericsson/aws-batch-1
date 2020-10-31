3ba177918e69a6c137e93413d02150743a0153fb
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-10-31 15:58:49 +0530

    adding outputs to be printed on Screen

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "ericsson_server_public_IP" {
 	value = aws_instance.ericsson_server.*.public_ip
 }
+
+output "ericsson_server_public_DNS" {
+	value = aws_instance.ericsson_server.*.public_dns
+}
+
+output "ericsson_server_private_IP" {
+	value = aws_instance.ericsson_server.*.private_ip
+}
+
+output "ericsson_vol_IDs" {
+	value = aws_ebs_volume.ericsson-vol.*.id
+}
