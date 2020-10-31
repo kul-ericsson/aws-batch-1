09d536530c270c7f6e12cfea2b53df9abf717756
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-10-31 14:12:07 +0530

    added remote-exec provisioner, to mount EBS on Server

--- resources_volume.tf
+++ resources_volume.tf
@@ -12,4 +12,18 @@ resource "aws_volume_attachment" "attach-vol" {
 	volume_id = aws_ebs_volume.ericsson-vol[count.index].id
 	instance_id = aws_instance.ericsson_server[count.index].id
 	device_name = "/dev/sdf"
+	
+	provisioner "remote-exec" {
+		connection {
+			type = "ssh"
+			user = "centos"
+			private_key = file("C:/training/ericsson/aws/batch-1/ericsson-kul.pem")
+			host = aws_instance.ericsson_server[count.index].public_ip
+		}
+		inline = [
+			"sudo mkfs -t ext4 /dev/xvdf",
+			"sudo mkdir /data",
+			"sudo mount /dev/xvdf /data"
+		]
+	}	
 }
