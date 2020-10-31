4f5d95c3d47e3e892162a66dc4d86f4b888a079a
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-10-31 12:24:33 +0530

    updated to add User Defined Security Group & reduced to count to 1 for fast execution

--- resources.tf
+++ resources.tf
@@ -2,10 +2,13 @@ resource "aws_instance" "ericsson_server" {
 	ami = "ami-01e36b7901e884a10"
 	instance_type = "t2.micro"
 	key_name = "ericsson-kul"
-	count = 2
+	count = 1
 	tags = {
 		Name = "Ericsson-Kul"
 	}
+	security_groups = [
+		aws_security_group.ericsson_sg.name
+	]
 }
 
 resource "aws_security_group" "ericsson_sg" {
